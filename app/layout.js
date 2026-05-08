0a69e8bbd848b1faf9726e5eac3011c981b47ac7
datogamrekelidze-debug <dato.gamrekelidze@gmail.com>
2026-05-08 19:30:34 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,11 @@
 import "./globals.css";
+import { Space_Grotesk } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-space"
+});
 
 export const metadata = {
   title: "Padel Group | Courts. Clubs. Community.",
@@ -7,8 +14,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ka">
-      <body>{children}</body>
+    <html lang="en">
+      <body className={spaceGrotesk.variable}>
+        {children}
+      </body>
     </html>
   );
 }
